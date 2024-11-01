45eefa24ed1225dbd704166e2dc655eb4bc83f3e
max 54321 <maxymk17@gmail.com>
2024-11-01 02:01:57 +0200

    Completed Database ,check it

---
+++ ClientApp/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ClientApp.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public bool IsExpense { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
+
